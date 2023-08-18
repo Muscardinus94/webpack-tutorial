80cbe68cfc984fdfafb8d999cbad4ea48eb013f0
Jae Kim <9028364@gmail.com>
2023-08-19 01:31:20 +0900

    Section4: Loaders

--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,31 @@ module.exports = {
         },
       },
       {
-        test: /\.txt/,
+        test: /\.txt$/,
         type: "asset/source",
+      },
+      {
+        test: /\.css$/,
+        // style-loader = css를 js에 주입한다
+        // css-loader = css를 읽는다
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            // es5로 다 변환
+            presets: ["@babel/env"],
+            // class는 es에서 아직 정식으로 인정되지 않음
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+        },
       },
     ],
   },
